878c09aa82b1e9a6388158087c18257ceb367793
Samuel Penney <scpenney2@gmail.com>
2025-01-14 22:00:05 -0700

    Making changes to how people get infected

---
+++ TestingNewInfectionMethod.py
@@ -0,0 +1,57 @@
+import random
+Population = int(input("Enter the population: "))
+Houses = (Population // 3) + (Population % 3)
+
+'''
+people = []
+Households = []
+for i in range(Population):
+    people.append(f"Person{i + 1}")
+
+for i in range(Houses):
+    home = []
+    for j in range(3):
+        if people:
+            human = people.pop(0)
+            home.append(human)
+    Households.append(home)
+'''
+class Person():
+    def __init__(self, name):
+        self.name = name
+        self.infected = False
+        self.infected_days = 0
+    Morning = None
+    Midday = None
+    Evening = None
+    Transit = None
+class NTFR(Person):
+    Morning = random.randint(1, 7)
+    Midday = random.randint(1, 7)
+    Evening = random.randint(1, 7)
+
+def CreatePopulation(people):
+    houses = (people // 3) + (people % 3)
+    humans = []
+    Households= []
+    for i in range(people):
+        humans.append(NTFR(f"Person{i + 1}"))
+    for i in range(houses):
+        home = []
+        for j in range(3):
+            if humans:
+                home.append(humans.pop(0))
+        Households.append(home)
+    return Households
+
+def InfectHuman(HouseHolds):
+    number = random.randint(0, len(Households))
+    number2 = random.randint(0, 2)
+    Households[number][number2].infected = True
+
+Households = CreatePopulation(Population)
+
+for i in range(len(Households)):
+    print(f"Household {i + 1}:")
+    for j in range(len(Households[i])):
+        print(f"\t{Households[i][j].infected}")
