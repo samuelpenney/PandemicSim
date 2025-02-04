c1f600a30dd5823e0a86a88d5f0f8704e17de7a6
Samuel Penney <scpenney2@gmail.com>
2025-02-03 19:01:20 -0700

    trying to make a graph

---
+++ GraphRepresentationTesting.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.ticker as mticker
+import time
+
+
--- PandemicSim2.py
+++ PandemicSim2.py
@@ -1,5 +1,7 @@
 import random
 
+
+
 class Person:
     def __init__(self, name):
         self.name = name
